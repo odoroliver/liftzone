448db6b8055fb74deaf5beed74811745cbbc453e
odoroliver <71766082+odoroliver@users.noreply.github.com>
2024-10-07 14:53:17 +0200

    Add files via upload

---
+++ liftzone/arak.php
@@ -0,0 +1,119 @@
+<head>
+    <link rel="stylesheet" href="arak.css">
+</head>
+<body>
+<div class="arak">
+    <img src="kepek/bg.jpg" alt="as">
+    <h1 class="text-warning">Bérletárak</h1>
+
+    <div class="row">
+        <!-- Felnőtt -->
+        <div class="col-md-5">
+            <h2 class="section-title">Felnőtt</h2>
+            <table class="table table-borderless">
+                <tr>
+                    <th>Típus</th>
+                    <th>Ár</th>
+                </tr>
+                <tr>
+                    <td>Havi bérlet</td>
+                    <td>19 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Napijegy</td>
+                    <td>2 990 Ft</td>
+                </tr>
+                <tr>
+                    <td>Három hónapos bérlet</td>
+                    <td>49 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Hat hónapos bérlet</td>
+                    <td>99 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Éves bérlet</td>
+                    <td>199 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Egy hetes bérlet</td>
+                    <td>12 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Tíz alkalmas bérlet*</td>
+                    <td>17 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Délelőtti bérlet</td>
+                    <td>16 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Hétvégi bérlet</td>
+                    <td>12 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Törölköző használat (Kicsi)</td>
+                    <td>350 Ft</td>
+                </tr>
+                <tr>
+                    <td>Törölköző használat (Nagy)</td>
+                    <td>450 Ft</td>
+                </tr>
+            </table>
+        </div>
+
+        <!-- Diák/Nyugdíjas -->
+        <div class="col-md-5">
+            <h2 class="section-title">Diák / Nyugdíjas</h2>
+            <table class="table table-borderless">
+                <tr>
+                    <th>Típus</th>
+                    <th>Ár</th>
+                </tr>
+                <tr>
+                    <td>Havi bérlet</td>
+                    <td>17 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Napijegy</td>
+                    <td>2 490 Ft</td>
+                </tr>
+                <tr>
+                    <td>Három hónapos bérlet</td>
+                    <td>45 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Hat hónapos bérlet</td>
+                    <td>89 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Éves bérlet</td>
+                    <td>176 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Egy hetes bérlet</td>
+                    <td>11 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Tíz alkalmas bérlet*</td>
+                    <td>15 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Délelőtti bérlet</td>
+                    <td>15 900 Ft</td>
+                </tr>
+                <tr>
+                    <td>Törölköző használat (Kicsi)</td>
+                    <td>350 Ft</td>
+                </tr>
+                <tr>
+                    <td>Törölköző használat (Nagy)</td>
+                    <td>450 Ft</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+    <p><i>*Az alkalmas bérletek a vásárlástól számított 60 napig érvényesek.</i></p>
+</div>
+</body>
